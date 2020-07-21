232ee84472ce9b9259c1cc7c207eeeda424fea9f
Pasha1968 <42270001+Pasha1968@users.noreply.github.com>
2020-07-21 13:51:30 +0300

    Add files via upload

---
+++ struct.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type person struct {
+	age  int
+	name string
+}
+
+type worker struct {
+	man      person
+	jobname  string
+	location string
+}
+
+func birth(man person) {
+	currentTime := time.Now()
+	year := currentTime.Year()
+	fmt.Println(year - man.age)
+}
+func main() {
+	var me = worker{
+		jobname:  "student",
+		location: "Dnepr",
+		man: person{
+			name: "Pasha",
+			age:  19,
+		},
+	}
+	fmt.Println(me)
+
+	var he worker
+	fmt.Println(he)
+	he.man.age = 50
+	fmt.Println(he)
+
+	it := worker{jobname: "pet", location: "Kiev"}
+	it.man.name = "Pes"
+	fmt.Println("it's name:", it.man.name)
+
+	itt := worker{me.man, "tep", "Dnepr"}
+	fmt.Println(itt)
+
+	she := new(worker)
+	fmt.Println(she.man.age)
+	birth(me.man)
+}
